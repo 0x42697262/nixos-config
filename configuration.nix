39c792f7a53790c6d43cb64443c9ddc9c2898f33
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2024-03-31 06:36:10 +0900

    moved stuffs around, added nvidia

--- configuration.nix
+++ configuration.nix
@@ -110,63 +110,70 @@
     extraGroups = [ "wheel" "video" "networkmanager" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       # appimage-run
-      dunst
-      thunderbird
       # geekbench_6
+      # metasploit
+      dunst
       grimblast
       lazygit
       lynx
-      # metasploit
       neofetch
+      nvtop
       pavucontrol
       rofi
       slurp
+      thunderbird
       tree
       waybar
-      osu-lazer-bin
     ];
   };
 
-  programs = {
-    hyprland = {
-      enable = true;
-      xwayland.enable = true;
-    };
-    firejail = {
-      enable = true;
-      wrappedBinaries = {
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
 
-        chromium = {
-          executable = "${pkgs.chromium}/bin/chromium";
-          extraArgs = [
-            "--private=~/firejail"
-            "--noprofile"
-          ];
-        };
+  };
+  programs.firejail = {
+    enable = true;
+    wrappedBinaries = {
 
-        appimage-run = {
-          executable = "${pkgs.appimage-run}/bin/appimage-run";
-          extraArgs = [
-            "--private=~/firejail"
-            "--noprofile"
-          ];
-        };
+      osu = {
+        executable = "${pkgs.osu-lazer-bin}/bin/osu!";
+        extraArgs = [
+          "--private=~/firejail"
+          "--noprofile"
+        ];
+      };
 
-        geekbench = {
-          executable = "${pkgs.geekbench}/bin/geekbench6";
-          extraArgs = [
-            "--private=~/firejail"
-            "--noprofile"
-          ];
-        };
+      chromium = {
+        executable = "${pkgs.chromium}/bin/chromium";
+        extraArgs = [
+          "--private=~/firejail"
+          "--noprofile"
+        ];
+      };
 
-        metasploit = {
-          executable = "${pkgs.metasploit}/bin/metasploit";
-          extraArgs = [
-            "--private=~/firejail"
-            "--noprofile"
-          ];
-        };
+      appimage-run = {
+        executable = "${pkgs.appimage-run}/bin/appimage-run";
+        extraArgs = [
+          "--private=~/firejail"
+          "--noprofile"
+        ];
+      };
+
+      geekbench = {
+        executable = "${pkgs.geekbench}/bin/geekbench6";
+        extraArgs = [
+          "--private=~/firejail"
+          "--noprofile"
+        ];
+      };
+
+      metasploit = {
+        executable = "${pkgs.metasploit}/bin/metasploit";
+        extraArgs = [
+          "--private=~/firejail"
+          "--noprofile"
+        ];
       };
     };
   };
@@ -210,6 +217,31 @@
     daemon.enable = true;
   };
 
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  hardware.nvidia = {
+    open = false;
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+    prime.amdgpuBusId = "PCI:0:6:0";
+    prime.nvidiaBusId = "PCI:0:1:0";
+    prime.offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+  };
+
+  services.xserver = {
+    videoDrivers = [ "nvidia" ];
+  };
+
   # List services that you want to enable:
   services.automatic-timezoned.enable = true;
   # Enable the OpenSSH daemon.
@@ -222,24 +254,22 @@
     NetworkManager-wait-online.enable = false;
   };
 
-
-
-  boot.extraModprobeConfig = ''
-    blacklist nouveau
-    options nouveau modeset=0
-  '';
-
-  services.udev.extraRules = ''
-    # Remove NVIDIA USB xHCI Host Controller devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA USB Type-C UCSI devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA Audio devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA VGA/3D controller devices
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-  '';
-  boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+  # boot.extraModprobeConfig = ''
+  #   blacklist nouveau
+  #   options nouveau modeset=0
+  # '';
+  #
+  # services.udev.extraRules = ''
+  #   # Remove NVIDIA USB xHCI Host Controller devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA USB Type-C UCSI devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA Audio devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA VGA/3D controller devices
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+  # '';
+  # boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
