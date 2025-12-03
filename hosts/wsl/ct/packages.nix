6c6ba5f80b79108b9ff548a893f9ebd763c887af
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2025-12-03 23:11:12 +0000

    add gh cli

--- hosts/wsl/ct/packages.nix
+++ hosts/wsl/ct/packages.nix
@@ -1,7 +1,9 @@
 { config, libs, pkgs, ...}:
 
 {
-  environment.systemPackages = with pkgs; [];
+  environment.systemPackages = with pkgs; [
+    gh
+  ];
   programs._1password = {
     enable = true;
   };
