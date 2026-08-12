ae87869abb1257b134fe662540c364d03f841f3b
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2026-08-13 02:00:20 +0900

    hosts/owasp: add tools to owasp host

--- hosts/owasp/default.nix
+++ hosts/owasp/default.nix
@@ -5,5 +5,11 @@
 
   networking.hostName = "owasp";
 
+  myProfiles.security = {
+    web.enable = true;
+    reversing.enable = true;
+    crypto.enable = true;
+  };
+
   system.stateVersion = "26.11";
 }
